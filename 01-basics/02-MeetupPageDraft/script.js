7c3520574afce4f8fe84985bdb75d36c3c0ae32e
Vladimir Krasnikov <krasv2@gmail.com>
2020-12-27 14:30:20 -0700

    01-basics-02-MeetupPageDraft and 02-components-01-CounterButton Added

--- 01-basics/02-MeetupPageDraft/script.js
+++ 01-basics/02-MeetupPageDraft/script.js
@@ -15,6 +15,9 @@ function getMeetupCoverLink(meetup) {
   return `${API_URL}/images/${meetup.imageId}`;
 }
 
+// const fetchMeetupById = () => fetch(`${API_URL}/meetups/${MEETUP_ID}`).then((res) => res.json());
+
+
 /**
  * Словарь заголовков по умолчанию для всех типов элементов программы
  */
@@ -44,23 +47,61 @@ const agendaItemIcons = {
   other: 'cal-sm',
 };
 
+const getDateOnlyString = (date) => {
+  
+  const YYYY = date.getUTCFullYear();
+  const MM = (date.getUTCMonth() + 1).toString().padStart(2,'0');
+  const DD = date.getUTCDate().toString().padStart(2,'0');
+
+  return `${YYYY}-${MM}-${DD}`;
+};
+
 export const app = new Vue({
   el: '#app',
 
   data: {
     //
+    rawMeetup: null,
   },
 
-  mounted() {
+ async mounted() {
     // Требуется получить данные митапа с API
+    this.rawMeetup = await this.getMeetupById(MEETUP_ID);
   },
 
   computed: {
     //
+    meetup() {
+      if(!this.rawMeetup){
+        return null;
+      }
+      
+      return { 
+        ...this.rawMeetup,
+        agenda: this.rawMeetup.agenda ? this.rawMeetup.agenda.map((item) => ({
+          ...item,
+          title: item.title ? item.title : agendaItemTitles[item.type],
+          icon: agendaItemIcons[item.type] ? 'icon-' + agendaItemIcons[item.type] + '.svg' : 'icon-cal-sm.svg',
+        })) : [],
+        cover: this.rawMeetup.imageId ? getMeetupCoverLink(this.rawMeetup) : undefined,
+        localDate: new Date(this.rawMeetup.date).toLocaleString(navigator.language, {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        }),
+        // utcDate: this.rawMeetup.date,
+        utcDate: getDateOnlyString(new Date(this.rawMeetup.date)),
+      };
+    }
   },
 
   methods: {
     // Получение данных с API предпочтительнее оформить отдельным методом,
     // а не писать прямо в mounted()
+    getMeetupById: async function(meetupId) {
+      const res = await fetch(`${API_URL}/meetups/${meetupId}`);
+      return await res.json();
+    },
+
   },
 });
--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,28 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="incrementCounter">{{ count }}</button>',
 
   // Компонент должен иметь входной параметр
-
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
   // Компонент должен иметь модель
-
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+
+    incrementCounter() {
+
+      let value = this.count + 1;
+      this.$emit('increment', value);
+
+    }
+    
+  },
 };
