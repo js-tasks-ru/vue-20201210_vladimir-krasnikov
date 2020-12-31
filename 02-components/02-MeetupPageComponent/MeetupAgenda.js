7aef8b0a55397838ea5dc7f30712920980dbb55e
Vladimir Krasnikov <krasv2@gmail.com>
2020-12-31 01:52:42 -0700

    02-components - 02-MeetupPageComponent - Added

--- 02-components/02-MeetupPageComponent/MeetupAgenda.js
+++ 02-components/02-MeetupPageComponent/MeetupAgenda.js
@@ -1,14 +1,22 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { MeetupAgendaItem } from './MeetupAgendaItem.js';
 
 export const MeetupAgenda = {
   name: 'MeetupAgenda',
 
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
+        <meetup-agenda-item v-for="item in agenda" :key="item.id" :agendaItem="item"></meetup-agenda-item>
     </div>`,
 
   // components
+  components: {
+    MeetupAgendaItem,
+  },
 
   // props
+  props: {
+    agenda: {
+      type: Array,
+    },
+  },
 };
--- 02-components/02-MeetupPageComponent/MeetupCover.js
+++ 02-components/02-MeetupPageComponent/MeetupCover.js
@@ -1,7 +1,25 @@
+import {getMeetupCoverLink} from './data.js';
+
 export const MeetupCover = {
-  template: `<div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Название митапа</h1>
+  template: `<div class="meetup-cover" :style="coverSyle">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 
   // props
+  props: {
+    link: {
+      type: String,
+    },
+    title: {
+      type: String,
+    }
+
+  },
+  
+  computed: {
+    coverSyle() {
+      return this.link ? "--bg-url: url('" + this.link + "')" : "";
+      //--bg-url: url('https://course-vue.javascript.ru/api/images/2')
+    },
+  },
 };
