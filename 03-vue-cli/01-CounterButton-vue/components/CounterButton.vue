ef1af18cf17f99e67dcc79aeab4c9a365e44f23c
Vladimir Krasnikov <krasv2@gmail.com>
2021-01-04 23:49:16 -0700

    03-vue-cli - 01-CounterButton Added

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,30 @@
-<template><div></div></template>
+<template>
+  <div>
+    <button type="button" @click="incrementCounter">{{ count }}</button>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  methods: {
+    incrementCounter() {
+      let value = this.count + 1;
+      this.$emit('increment', value);
+    },
+  },
+};
 </script>
 
 <style></style>
